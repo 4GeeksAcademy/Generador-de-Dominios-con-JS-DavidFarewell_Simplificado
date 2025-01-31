ce5caf0a7ba7c3a463c8ce583d21362ccbb69535
DavidFarewell <david.castillo.architect@gmail.com>
2025-01-31 19:55:33 +0000

    Generador de Dominios Random - DavidFarewell

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronouns = ["the", "our"];
+  const adjectives = ["great", "big"];
+  const nouns = ["jogger", "raccoon"];
+  const extension = ".com";
+
+  const button = document.getElementById("boton");
+  const domainList = document.getElementById("domainList");
+
+  button.addEventListener("click", generateDomains);
+
+  function generateDomains() {
+    domainList.innerHTML = "";
+
+    let domains = pronouns
+      .map(pronoun =>
+        adjectives.map(adjective =>
+          nouns.map(noun => `${pronoun}${adjective}${noun}${extension}`)
+        )
+      )
+      .flat(2);
+
+    domains.forEach((domain, index) => {
+      let li = document.createElement("li");
+      li.className = "list-group-item";
+      li.textContent = `${index + 1}. ${domain}`;
+      domainList.appendChild(li);
+    });
+  }
 };
